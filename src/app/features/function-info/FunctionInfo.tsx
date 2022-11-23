8368cffdfe4f59cb90d57efe68334667450a16e9
Brandon <btnewcomer@pm.me>
2022-11-23 08:57:41 -0600

    reverting typescript update
    compatibility issue with react-use caused breaking changes.

--- src/app/features/function-info/FunctionInfo.tsx
+++ src/app/features/function-info/FunctionInfo.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import useEvent from "react-use/lib/usePrevious";
+import { useEvent } from "react-use";
 
 import FunctionDetails from "./FunctionDetails";
 import FunctionList from "./FunctionList";
